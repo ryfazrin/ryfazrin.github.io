d5aa2b6019302a53479eede553a24d99e1e3ada6
ryfazrin <riansera515@gmail.com>
2023-02-21 00:46:53 +0700

    updated: data json

--- src/data/index.js
+++ src/data/index.js
@@ -1,11 +1,11 @@
 import bio from './bio.json'
 import resume from './resume.json'
 import projects from './projects.json'
-import certification from './certification.json'
+import certificates from './certificates.json'
 
 export default {
   bio,
   resume,
   projects,
-  certification
+  certificates
 }
