71631d67e483313a2d9f6eee4f626099fd1328be
ryfazrin <riansera515@gmail.com>
2023-02-21 00:33:14 +0700

    created: data json

---
+++ src/data/index.js
@@ -0,0 +1,11 @@
+import bio from './bio.json'
+import resume from './resume.json'
+import projects from './projects.json'
+import certification from './certification.json'
+
+export default {
+  bio,
+  resume,
+  projects,
+  certification
+}
