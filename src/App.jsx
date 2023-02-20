e0fa82275eef7599dbac540a3ee884c59e777967
ryfazrin <riansera515@gmail.com>
2023-02-20 17:05:13 +0700

    added: template

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,118 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <button className="btn">Button</button>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div className="App container px-2 md:artboard artboard-horizontal phone-5 mx-auto">
+      {/* NAVBAR */}
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a>Home</a></li>
+              <li><a>About</a></li>
+              <li><a>Contact</a></li>
+              <li><a>Resume</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl md:text-3xl">
+            <span className="text-primary">Paz</span>
+            <span className="text-base-content">rin</span>
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+            <li><a>Resume</a></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Hire Me</a>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* Header start */}
+      <div className="w-full p-1">
+        <div className="text-5xl leading-relaxed">
+          <p>Hello 👋</p>
+          <p>I'm Muhammad Pazrin Andreanor - curious</p>
+          <p>Frontend Development Engineer</p>
+        </div>
+        <ul className="menu menu-horizontal bg-base-200 rounded-box p-2 mt-2">
+          <li><a>Github</a></li>
+          <li><a>LinkedIn</a></li>
+          <li><a>Blog</a></li>
+          <li><a>Email</a></li>
+        </ul>
+      </div>
+
+      <div className="divider"></div>
+
+      {/* Works */}
+      <h1>Works</h1>
+      <div className="w-full grid grid-cols-1 gap-4">
+        <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title text-xl font-medium">
+            Focus me to see content
+          </div>
+          <div className="collapse-content">
+            <div className="hero-content flex-col lg:flex-row">
+              <img src="https://via.placeholder.com/1000x700" className="max-w-sm rounded-lg shadow-2xl" />
+              <div>
+                <h1 className="text-5xl font-bold">Box Office News!</h1>
+                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <button className="btn">Get Started</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider"></div>
+
+      {/* Certification */}
+      <h1>Certification</h1>
+      <div className="w-full grid grid-cols-3 gap-4">
+        <div className="card col-span-1 bg-base-100 shadow-xl">
+          <figure className="px-5 pt-10">
+            <img src="https://via.placeholder.com/300x170" alt="Shoes" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider"></div>
+
+      {/* Contact */}
+      <div></div>
+
+      {/* Footer   */}
+      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <div className="grid grid-flow-col gap-4">
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+          </div>
+        </div>
+        <div>
+          <p>Copyright © 2023 - with love Ryfazrin</p>
+        </div>
+      </footer>
     </div>
   )
 }
