acd17009bad18dc8a475211e25e051268afc0564
ryfazrin <riansera515@gmail.com>
2023-02-21 00:32:55 +0700

    updated: template head in section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import githubLogo from './assets/images/github-logo.png'
 
 function App() {
   return (
@@ -39,21 +40,30 @@ function App() {
       <div className="w-full p-1">
         <div className="text-5xl leading-relaxed">
           <p>Hello 👋</p>
-          <p>I'm Muhammad Pazrin Andreanor - curious</p>
+          <p>I'm Muhammad Pazrin Andreanor</p>
           <p>Frontend Development Engineer</p>
         </div>
-        <ul className="menu menu-horizontal bg-base-200 rounded-box p-2 mt-2">
-          <li><a>Github</a></li>
-          <li><a>LinkedIn</a></li>
-          <li><a>Blog</a></li>
-          <li><a>Email</a></li>
-        </ul>
+        <div className="flex justify-between items-center">
+          <ul className="menu menu-horizontal bg-base-200 rounded-box p-2 mt-2">
+            <li><a>Github</a></li>
+            <li><a>LinkedIn</a></li>
+            <li><a>Blog</a></li>
+            <li><a>Email</a></li>
+          </ul>
+          <button className="btn btn-outline rounded-full">Download CV</button>
+        </div>
+      </div>
+
+      {/* About */}
+      <h1 className="text-2xl my-4">About</h1>
+      <div className="w-full">
+        <p>Saya memiliki keahlian dalam pengembangan Web Frontend khususnya Framework Vuejs, Reactjs dan pengembangan Tool untuk Frontend. Selama 1 tahun saya menjalani magang pada e-commerce blibli pada bagian Frontend divisi Research & Development. Saya berhasil mengembangkan Integration Test Tools untuk tim developer Frontend blibli. Di divisi yang sama, saya melakukan research untuk meningkatkan performa halaman blibli.</p>
       </div>
 
       <div className="divider"></div>
 
       {/* Works */}
-      <h1>Works</h1>
+      <h1 className="text-2xl my-4">Works</h1>
       <div className="w-full grid grid-cols-1 gap-4">
         <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
           <input type="checkbox" className="peer" />
@@ -76,7 +86,7 @@ function App() {
       <div className="divider"></div>
 
       {/* Certification */}
-      <h1>Certification</h1>
+      <h1 className="text-2xl my-4">Certification</h1>
       <div className="w-full grid grid-cols-3 gap-4">
         <div className="card col-span-1 bg-base-100 shadow-xl">
           <figure className="px-5 pt-10">
@@ -97,16 +107,13 @@ function App() {
       {/* Footer   */}
       <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <div className="grid grid-flow-col gap-4">
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">LinkedIn</a>
+          <a className="link link-hover">Blog</a>
+          <a className="link link-hover">Resume</a>
         </div>
         <div>
           <div className="grid grid-flow-col gap-4">
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+            <a><img className="w-20" src={githubLogo} /></a>
           </div>
         </div>
         <div>
