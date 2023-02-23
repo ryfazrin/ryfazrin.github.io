b1c1510610cdfd67fa7fccf34255aa62338a7588
ryfazrin <riansera515@gmail.com>
2023-02-23 23:17:58 +0700

    added: social in burger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import About from './components/about'
-import Certifications from './components/certifications'
 import Contact from './components/contact'
 import Divider from './components/divider'
 import Footer from './components/footer'
@@ -14,7 +13,7 @@ function App() {
   return (
     <div className="App mx-auto md:mockup-window md:border bg-base-300 md:my-2 mx-2">
       <div className="md:px-16 md:py-4 max-[640px]:px-4 bg-base-100">
-        <Navbar />
+        <Navbar bio={bio} />
         <Header bio={bio} />
         <About aboutMe={bio.aboutMe} />
         <Divider />
